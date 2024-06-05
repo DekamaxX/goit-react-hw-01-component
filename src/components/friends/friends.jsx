77f55763c6f1be809a596abce2029e14fb2fb9d0
DekamaxX <dobosz.mateusz1994@gmail.com>
2024-06-05 10:01:06 +0200

    add friendslist

---
+++ src/components/friends/friends.jsx
@@ -0,0 +1,31 @@
+import css from './friends.module.css';
+import propTypes from 'prop-types';
+
+export const FriendsList = ({ friends, index }) => {
+  return (
+    <ul className={css.friends}>
+      {friends.map(friend => (
+        <li className={css.item} key={friend.id || index}>
+          <span
+            className={
+              friend.isOnline ? css['statusOnline'] : css['statusOffline']
+            }
+          ></span>
+          <img
+            className={css.avatar}
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={css.name}>{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendsList.propTypes = {
+  id: PropTypes.number,
+  avatar: PropTypes.string,
+  isOline: PropTypes.bool,
+};
